1bb923efdc21928127fa935e677897f975d2bf38
lyhuynhnhu <lyhuynhnhu200699@gmail.com>
2021-05-13 22:33:06 +0700

    L_Principle

--- src/LiskovSubstitution/Liskov_Substitution.java
+++ src/LiskovSubstitution/Liskov_Substitution.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package LiskovSubstitution;
 
-/**
- *
- * @author ACER
- */
 public class Liskov_Substitution {
-  
+  public static void main(String[] args) {
+    Crawling_Animal crawling = new Snake();
+    crawling.print();
+
+    Jumping_Animal jumping = new Frog();
+    jumping.print();
+  }
 }
+
+class Crawling_Animal {
+  public void print() {
+    System.out.println("This is a crawling animal");
+  }
+}
+
+class Jumping_Animal {
+  public void print() {
+    System.out.println("This is a jumping animal");
+  }
+}
+
+class Snake extends Crawling_Animal {
+  //do another func
+}
+ class Frog extends Jumping_Animal {
+   //do another func
+ }
